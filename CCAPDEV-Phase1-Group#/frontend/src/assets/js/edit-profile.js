89095595778258f706b92f825d8cabdda5c1dc61
antqnluis <antqnluis@gmail.com>
2026-03-01 14:22:58 +0800

    feat: implement login/logout button logic and user session handling

--- CCAPDEV-Phase1-Group#/frontend/src/assets/js/edit-profile.js
+++ CCAPDEV-Phase1-Group#/frontend/src/assets/js/edit-profile.js
@@ -1,5 +1,19 @@
 const form = document.getElementById("editProfileForm");
 const user = JSON.parse(localStorage.getItem("loggedUser"));
+const logBtn = document.getElementById("log-btn");
+
+if (!user) {
+    logBtn.textContent = "Join Us";
+    logBtn.href = "register.html";
+} else {
+    logBtn.textContent = "Logout";
+    logBtn.href = "logout.html";
+    logBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        localStorage.removeItem("loggedUser");
+        window.location.href = "logout.html";
+    });
+}
 
 if (!user) {
     window.location.href = "../../pages/static-html/login.html";
