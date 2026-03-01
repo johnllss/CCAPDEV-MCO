f54ca38105543a868e928ff4285981105e5b4884
antqnluis <antqnluis@gmail.com>
2026-03-01 14:30:16 +0800

    feat: user session handling

--- CCAPDEV-Phase1-Group#/frontend/src/assets/js/view-post.js
+++ CCAPDEV-Phase1-Group#/frontend/src/assets/js/view-post.js
@@ -4,8 +4,27 @@ const deleteBtn = document.getElementById("delete-button");
 deleteBtn.addEventListener("click", () => {
     const delConfirmed = confirm("Are you sure you want to delete this post?\nThis action cannot be undone.");
 
-    if(delConfirmed) {
+    if (delConfirmed) {
         alert("[PLACEHOLDER] Post has been deleted successfully!");
         window.location.href = "index.html";
     }
 })
+
+// Login / Logout Button
+
+const user = JSON.parse(localStorage.getItem("loggedUser"));
+const logBtn = document.getElementById("log-btn");
+
+
+if (!user) {
+    logBtn.textContent = "Join Us";
+    logBtn.href = "register.html";
+} else {
+    logBtn.textContent = "Logout";
+    logBtn.href = "logout.html";
+    logBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        localStorage.removeItem("loggedUser");
+        window.location.href = "logout.html";
+    });
+}
