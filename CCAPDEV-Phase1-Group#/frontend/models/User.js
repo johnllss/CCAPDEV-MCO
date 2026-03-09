400ae982377f43043e295d126d8772f17048b31c
antqnluis <antqnluis@gmail.com>
2026-03-09 12:49:50 +0800

    refactor: update User model and enhance login functionality with API integration

--- CCAPDEV-Phase1-Group#/frontend/models/User.js
+++ CCAPDEV-Phase1-Group#/frontend/models/User.js
@@ -1,10 +1,24 @@
 const { Schema, model } = require('mongoose');
 const UserSchema = new Schema({
 
-    name: String,
     username: { type: String, unique: true },
     email: { type: String, unique: true },
-    password: String
+    password: String,
+
+    profile: {
+        fullname: { type: String, default: "" },
+        quote: { type: String, default: "" },
+        about: { type: String, default: "" },
+        photo: { type: String, default: "/images/default-profile.png" }
+    },
+
+    posts: { type: Number, default: 0 },
+    replies: { type: Number, default: 0 },
+
+    activity: {
+        type: Array,
+        default: []
+    }
 
 }, { timestamps: true });
 
