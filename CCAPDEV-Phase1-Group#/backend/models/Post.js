3ae3f7b2c8d2870bfd4552820874bec549321a65
jncdngs <jonchesterdonguines07@gmail.com>
2026-03-12 22:10:09 +0800

    add pulled changes

---
+++ CCAPDEV-Phase1-Group#/backend/models/Post.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+const PostSchema = new Schema({
+
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    title: {
+        type: String,
+        required: true,
+        maxLength: 150
+    },
+
+    image: {
+        type: String
+    },
+
+    body: {
+        type: String,
+        required: true
+    },
+
+    votes: {
+        type: Number,
+        default: 0
+    }
+
+}, { timestamps: true });
+
+module.exports = model('Post', PostSchema);
--- CCAPDEV-Phase1-Group#/frontend/src/assets/js/post.js
+++ CCAPDEV-Phase1-Group#/frontend/src/assets/js/post.js
@@ -5,30 +5,31 @@ const fileName = document.getElementById("file-upload-name");
 const cancelBtn = document.getElementById("cancel-button");
 const publishBtn = document.getElementById("publish-button");
 
+
 // Post title character counter
 titleInput.addEventListener("input", () => {
     const currentLength = titleInput.value.length;
     charCtr.textContent = `${currentLength}/150`;
 
-    if(currentLength >= 150) {
+    if (currentLength >= 150) {
         charCtr.style.color = "#fc6e6e";
     } else {
         charCtr.style.color = "lightgray";
-    }    
-});    
+    }
+});
 
 // Prompts the user to confirm cancelling a post
 cancelBtn.addEventListener("click", () => {
     const postCancel = confirm("Are you sure you want to go back?");
 
-    if(postCancel) {
+    if (postCancel) {
         window.location.href = "javascript:history.back()";
     }
 })
 
 // Prompts the user to confirm and alerts when posted successfully
 publishBtn.addEventListener("click", () => {
-    if(titleInput.value.trim() === "") {
+    if (titleInput.value.trim() === "") {
         alert("Please add a title.");
         titleInput.focus();
         return;
@@ -36,18 +37,35 @@ publishBtn.addEventListener("click", () => {
 
     const postConfirmed = confirm("Are you sure you want to publish this post?");
 
-    if(postConfirmed) {
-        // TODO: add post to database
-
+    if (postConfirmed) {
         alert("Post has been published successfully!");
         window.location.href = "index.html"; // TODO: link this to actual post page
     }
 });
 
 // Replaces blank span with the filename of uploaded image
-fileInput.addEventListener("change", function() {
-    if(this.files && this.files.length > 0) {
+fileInput.addEventListener("change", function () {
+    if (this.files && this.files.length > 0) {
         const name = this.files[0].name;
         fileName.textContent = name;
     }
 });
+
+// Login / Logout Button
+
+const user = JSON.parse(localStorage.getItem("loggedUser"));
+const logBtn = document.getElementById("log-btn");
+
+
+if (!user) {
+    logBtn.textContent = "Join Us";
+    logBtn.href = "register.html";
+} else {
+    logBtn.textContent = "Logout";
+    logBtn.href = "logout.html";
+    logBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        localStorage.removeItem("loggedUser");
+        window.location.href = "logout.html";
+    });
+}
