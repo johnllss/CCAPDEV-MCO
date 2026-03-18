c84ae31f11beab892c83feb5ef23fbaa22d486af
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2026-03-18 23:24:29 +0800

    update: author details in Post

--- CCAPDEV-MCO-RubberDuckGroup/models/Post.js
+++ CCAPDEV-MCO-RubberDuckGroup/models/Post.js
@@ -10,17 +10,9 @@ const PostSchema = new Schema({
     },
 
     author: {
-        type: String
+        type: Schema.Types.ObjectId,
+        ref: 'User'
     },
-
-    createdOn: {
-        type: Date,
-        default: Date.now
-    },
-
-    updatedOn: {
-        type: Date
-    }
-});
+}, { timestamps: true });
 
 module.exports = model('Post', PostSchema);
