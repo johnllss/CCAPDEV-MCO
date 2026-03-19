cfa2b179768781d2990d2065a0676a054e98d1fd
John Lloyd Sunga <johnlloydsunga033@gmail.com>
2026-03-19 22:11:16 +0800

    feat: func for getting comments by postId

--- CCAPDEV-MCO-RubberDuckGroup/controllers/commentController.js
+++ CCAPDEV-MCO-RubberDuckGroup/controllers/commentController.js
@@ -1,5 +1,11 @@
 const Comment = require('../models/Comment');
 
-exports.method = async (req, res) => {
+exports.getCommentsByPostId = async (req, res) => {
+    try {
+        const comments = await Comment.find({ post: req.params.postId }).sort({ createdAt: -1 });
 
+        return res.json(comments);
+    } catch (err) {
+        return res.status(500).json({ message: 'Server error' });
+    }
 };
