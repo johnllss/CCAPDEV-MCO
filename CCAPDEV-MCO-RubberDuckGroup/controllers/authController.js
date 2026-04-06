5fb37b4adedba761c1ae0997fefb8fde25b4f229
antqnluis <antqnluis@gmail.com>
2026-04-06 14:09:45 +0800

    added cookie support and session support to login / logout

--- CCAPDEV-MCO-RubberDuckGroup/controllers/authController.js
+++ CCAPDEV-MCO-RubberDuckGroup/controllers/authController.js
@@ -39,7 +39,7 @@ async function register(req, res) {
 
 async function login(req, res) {
     try {
-        const { login, password } = req.body;
+        const { login, password, remember } = req.body;
 
         const user = await User.findOne({
             $or: [
@@ -62,8 +62,58 @@ async function login(req, res) {
             });
         }
 
+        req.session.userId = user._id;
+        req.session.username = user.username;
+
+        if (remember) { // basically if the remember button isnt clicked it will set the default to expire on session close
+            req.session.cookie.maxAge = 1000 * 60 * 60 * 24 * 21;
+        } else {
+            req.session.cookie.expires = false;
+        }
+
         res.json({
             message: "Login successful",
+            username: user.username,
+            profile: {
+                photo: user.profile?.photo || "/images/default-pfp.png"
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+}
+
+async function logout(req, res) {
+    try {
+        req.session.destroy(err => {
+            if (err) {
+                return res.status(500).json({ message: "Logout failed" });
+            }
+            res.clearCookie('connect.sid');
+            res.json({ message: "Logged out successfully" });
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+}
+
+async function me(req, res) {
+    try {
+        if (!req.session.userId) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+        const user = await User.findById(req.session.userId).select('-password');
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.json({
             userId: user._id,
             username: user.username,
             profile: {
@@ -80,5 +130,7 @@ async function login(req, res) {
 
 module.exports = {
     register,
-    login
+    login,
+    logout,
+    me
 };
